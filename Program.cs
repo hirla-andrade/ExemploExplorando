57cbccb509d4d440e48019b064502e4cfb6e7a6e
hirla-andrade <hhirlaandrade@gmail.com>
2024-01-04 23:50:53 -0300

    conceitos de get e set

--- Program.cs
+++ Program.cs
@@ -3,5 +3,38 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Hirla";
 p1.Sobrenome = "Andrade";
-p1.Idade = 19;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Eduardo";
+p2.Sobrenome = "Nerves Queiroz";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Hirla";
+// p1.Sobrenome = "Andrade";
+// p1.Idade = 19;
+// p1.Apresentar();
